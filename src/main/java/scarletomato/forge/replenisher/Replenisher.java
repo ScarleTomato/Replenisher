379ce0c9463ff783281958d051d5488f5fee001a
Mike Yarbrough <Michael.Yarbrough@aimco.com>
2017-03-22 15:07:00 -0400

    set mod as server side only removed debugging

--- src/main/java/scarletomato/forge/replenisher/Replenisher.java
+++ src/main/java/scarletomato/forge/replenisher/Replenisher.java
@@ -1,23 +1,14 @@
 package scarletomato.forge.replenisher;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = Replenisher.MODID, version = Replenisher.VERSION)
+@Mod(modid = Replenisher.MODID, version = Replenisher.VERSION, acceptableRemoteVersions="*", name = "Replenisher")
 public class Replenisher
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "replenisher";
     public static final String VERSION = "1.0";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
-    }
     
     @EventHandler
     public void serverStart(FMLServerStartingEvent event)
