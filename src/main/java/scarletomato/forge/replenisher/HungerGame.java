d000153a14652632ce26522ffea2fd3687249a9f
Mike Yarbrough <Michael.Yarbrough@aimco.com>
2017-03-24 13:51:35 -0400

    added some pre game boosts to players

--- src/main/java/scarletomato/forge/replenisher/HungerGame.java
+++ src/main/java/scarletomato/forge/replenisher/HungerGame.java
@@ -5,16 +5,21 @@ import java.util.List;
 import java.util.Queue;
 
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameType;
 
 public class HungerGame implements Runnable {
 	private MinecraftServer server;
 	List<Cage> cages = new LinkedList<>();
+	private List<EntityPlayerMP> players;
 
 	public HungerGame(MinecraftServer server) {
 		this.server = server;
+		players = server.getPlayerList().getPlayers();
 	}
 
 	@Override
@@ -40,10 +45,12 @@ public class HungerGame implements Runnable {
 		GameType livingMode = GameType.parseGameTypeWithDefault(Replenisher.INSTANCE.config.getString(Configuration.LIVING_MODE), GameType.ADVENTURE);
 		Queue<String> spawns = new LinkedList<>(Replenisher.INSTANCE.config.getStringList(Configuration.GAME_SPAWNS));
 		
-		for(EntityPlayerMP pl : server.getPlayerList().getPlayers()){
+		for(EntityPlayerMP pl : players){
 			String s = spawns.poll();
 			BlockPos pos = Replenisher.fromString(s);
 			cages.add(new Cage(pl.getEntityWorld(), pos).create());
+			pl.addPotionEffect(new PotionEffect(Potion.REGISTRY.getObject(new ResourceLocation("minecraft","health_boost")), 200));
+			pl.addPotionEffect(new PotionEffect(Potion.REGISTRY.getObject(new ResourceLocation("minecraft","saturation")), 200));
 			pl.setGameType(livingMode);
 			Replenisher.tp(pl, pos);
 			spawns.add(s);
